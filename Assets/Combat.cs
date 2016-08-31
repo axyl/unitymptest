75c915d78b5580f1ee5062cde8a687d2014ccc04
Craig <craig.foley@gmail.com>
2016-08-31 15:56:12 +1000

    Combat Damage is now managed by just server.

--- Assets/Combat.cs
+++ Assets/Combat.cs
@@ -1,2 +1,5 @@
-﻿using UnityEngine;using System.Collections;public class Combat : MonoBehaviour {    // Public variables..    public const int maxHealth = 100;   //const - can't be changed.
-    public int health = maxHealth;    // Public method called by the bullet.    public void TakeDamage(int amount)    {        health -= amount;        if (health< 0)        {            health = 0;            Debug.Log("Dead");        }    }        // Use this for initialization	void Start () {		}		// Update is called once per frame	void Update () {		}}
+﻿using UnityEngine;using UnityEngine.Networking;
+
+public class Combat : NetworkBehaviour {    // Public variables..    public const int maxHealth = 100;   //const - can't be changed.
+    [SyncVar]       // Synchronise this value between all clients and server.
+    public int health = maxHealth;    // Public method called by the bullet.    public void TakeDamage(int amount)    {        // only run on the server.        if (!isServer) return;        health -= amount;        if (health< 0)        {            health = 0;            Debug.Log("Dead");        }    }        // Use this for initialization	void Start () {		}		// Update is called once per frame	void Update () {		}}
