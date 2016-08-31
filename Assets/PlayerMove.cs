9651215789f264da7156e696cad4ae2a9d164ed9
Craig <craig.foley@gmail.com>
2016-08-31 10:02:40 +1000

    Added Bullets
    Local client only, not networked yet.

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -3,8 +3,10 @@ using UnityEngine.Networking;
 
 public class PlayerMove : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject bulletPrefab;    // Public...link this via the Unity UI
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 
@@ -26,5 +28,31 @@ public class PlayerMove : NetworkBehaviour {
         var z = Input.GetAxis("Vertical") * 0.1f;
 
         transform.Translate(x, 0, z);	
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
 	}
+
+    /// <summary>
+    /// Fire method if player is firing...
+    /// 
+    /// Called by Update if the user has pressed the fire button
+    /// 
+    /// </summary>
+    void Fire()
+    {
+        // Create the new bullet object from the prefab...
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,   
+            transform.position - transform.forward,
+            Quaternion.identity);
+
+        // Give it some velocity away from the player...
+        bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
+
+        // Destroy the bullet after two seconds  - What if it's already destroyed?
+        Destroy(bullet, 2.0f);
+    }
 }
